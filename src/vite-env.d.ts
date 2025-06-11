6725246b6973de2e04d96fa2643e5eda6653c1f6
karimsemmoud <karim.semmoud@netoum.com>
2025-06-11 20:16:44 +0200

    fix deploy

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+    readonly VITE_PAGEFIND?: string;
+    // add other env variables as needed
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
